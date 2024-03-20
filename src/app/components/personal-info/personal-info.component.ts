4c95130f0c7c111af228e158ba9b97122a5a8aa5
Feodor Kachinsky <fedor15zil@gmail.com>
2024-03-20 11:05:20 +0300

    fix remembering socials when moving from other page

--- src/app/components/personal-info/personal-info.component.ts
+++ src/app/components/personal-info/personal-info.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { Step } from '../register-form-container/register-form-container.component';
+import { FormGroup, FormBuilder, ReactiveFormsModule, FormArray } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { CardModule, FormModule, GridModule } from '@coreui/angular';
 
@@ -20,18 +19,65 @@ export class PersonalInfoComponent implements OnInit {
   constructor(private _fb: FormBuilder) {}
  
   ngOnInit() {
-    if (this.startingForm){
-      this.personalInfoForm = this._fb.group(this.startingForm)
+    if (this.startingForm) {
+
+      let startFormValue: any = this.startingForm;
+      this.personalInfoForm = this._fb.group({
+        name: startFormValue['name'],
+        age: startFormValue['age'],
+        city: startFormValue['city'],
+        socials: this._fb.array([]), // Initialize as empty FormArray
+      });
+  
+      // Populate socials FormArray with existing social media inputs
+      const socialsFormArray = this.personalInfoForm.get('socials') as FormArray;
+      const startingSocials = startFormValue.socials || [];
+      startingSocials.forEach((social: any) => {
+        // Check if the 'link' property is not empty before adding it to the FormArray
+        if (social.link.trim() !== '') {
+          const socialFormGroup = this._fb.group({
+            link: social.link, // Assuming each social object has a 'link' property
+          });
+          socialsFormArray.push(socialFormGroup);
+        }
+      });
+      
+      console.log(`this.personalInfoForm`,this.personalInfoForm)
+      
     } else {
       this.personalInfoForm = this._fb.group({
-        name: '',
-        age: '',
-        city: '',
-        socials: '',
-      })
+        name: [''],
+        age: [''],
+        city: [''],
+        socials: this._fb.array([]),
+      });
+      console.log(`new fomr`,this.personalInfoForm)
+      // this.addSocial(); // Add one initial social input
     }
     this.subformInitialized.emit(this.personalInfoForm);
   }
+
+  
+  
+  get socials(): FormArray {
+    console.log(`get socials  `,this.personalInfoForm.get('socials') as FormArray)
+    return this.personalInfoForm.get('socials') as FormArray;
+  }
+  
+  addSocial() {
+    // let socialFormGroup = new FormControl('')
+    const socialFormGroup = this._fb.group({
+      link: ['']
+    });
+    console.log(`this.socials is `,this.socials)
+    this.socials.push(socialFormGroup);
+  }
+  
+  removeSocial(index: number) {
+    this.socials.removeAt(index);
+  }
+  
+
   doChangeStep(direction: 'forward' | 'back') {
     this.changeStep.emit(direction);
   }

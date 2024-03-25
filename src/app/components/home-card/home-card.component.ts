b8dc74344d20789414f1f6f24dc33b6bbdb54ffa
Feodor Kachinsky <fedor15zil@gmail.com>
2024-03-25 16:05:36 +0300

    add basic home card

--- src/app/components/home-card/home-card.component.ts
+++ src/app/components/home-card/home-card.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CardModule } from '@coreui/angular';
 
 @Component({
   selector: 'app-home-card',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule
+  ],
   templateUrl: './home-card.component.html',
   styleUrl: './home-card.component.scss'
 })
